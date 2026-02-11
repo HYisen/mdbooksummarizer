2cb931271cc507e8f07ea1b0cd6e7e819cca8482
Alex <alexhyisen@gmail.com>
2026-02-11 22:42:27 +0800

    add Header Digester

--- digester.go
+++ digester.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"strings"
+)
 
-var Digester = FileSize
+var Digester DigesterFunc = Header
 
 type DigesterFunc func(content []byte) (title string)
 
+// FileSize use content bytes length as title for debug purpose.
+//
+//goland:noinspection GoUnusedExportedFunction
 func FileSize(content []byte) (title string) {
 	return fmt.Sprintf("filesize %d", len(content))
+}
+
+func Header(content []byte) (title string) {
+	scanner := bufio.NewScanner(bytes.NewReader(content))
+	scanner.Scan()
+	return ParseMdHeader(scanner.Text())
+}
+
+func ParseMdHeader(s string) string {
+	return strings.TrimLeft(s, "# ")
 }
